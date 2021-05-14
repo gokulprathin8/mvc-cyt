4047e2b24fc8071611cda7c0bc0e9df832e85f00
Gokul Prathin <gokulprathin8@gmail.com>
2021-05-15 04:33:54 +0530

    Completed Translation & Persistent Caching

--- controllers/translationController.js
+++ controllers/translationController.js
@@ -1,29 +1,66 @@
-const axios = require('axios').default;
 const Translation = require('../models/translation');
 const catchAsyncErrors = require('../utils/catchAsync');
+const { translate } = require('bing-translate-api');
 
 exports.getTextTranslation = catchAsyncErrors( async (req, res) => {
-    const options = {
-        method: 'POST',
-        url: 'https://yandextranslatezakutynskyv1.p.rapidapi.com/translate',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'x-rapidapi-key': '8c4a551693msh890dcfa5d9cdb64p1d6628jsnb1c178c40161',
-            'x-rapidapi-host': 'YandexTranslatezakutynskyV1.p.rapidapi.com'
-        },
-        data: {apiKey: req.body.apiKey, lang: req.body.language, text: req.body.textMessage}
-    };
+    const searchResult = await Translation.find({ textMessage: req.body.textMessage, fromLanguage: req.body.fromLanguage, toLanguage: req.body.toLanguage });
 
-    const newTranslation = await Translation.create(req.body);
-    axios.request(options).then(function (response) {
+    if (searchResult[0] === undefined) {
+        try {
+            const result = await translate(req.body.textMessage, req.body.fromLanguage, req.body.toLanguage, true);
+            const resultHindi = await translate(req.body.textMessage, req.body.fromLanguage, 'hi', true);
+            const resultTamil = await translate(req.body.textMessage, req.body.fromLanguage,  'ta', true);
+            const resultTelugu = await translate(req.body.textMessage, req.body.fromLanguage,  'te', true);
+
+            res.status(200).json({
+                status: 'success',
+                data: result
+            });
+            let myObj = {
+                textMessage: req.body.textMessage,
+                fromLanguage: req.body.fromLanguage,
+                toLanguage: req.body.toLanguage,
+                translatedMessage: result.translation
+            }
+            let myObjHindi = {
+                textMessage: req.body.textMessage,
+                fromLanguage: req.body.fromLanguage,
+                toLanguage: 'hi',
+                translatedMessage: resultHindi.translation
+            }
+            let myObjTamil = {
+                textMessage: req.body.textMessage,
+                fromLanguage: req.body.fromLanguage,
+                toLanguage: 'ta',
+                translatedMessage: resultTamil.translation
+            }
+            let myObjTelugu = {
+                textMessage: req.body.textMessage,
+                fromLanguage: req.body.fromLanguage,
+                toLanguage: 'ta',
+                translatedMessage: resultTelugu.translation
+            }
+
+            await Translation.create(myObj);
+            await Translation.create(myObjHindi);
+            await Translation.create(myObjTamil);
+            await Translation.create(myObjTelugu);
+
+
+
+        } catch (err) {
+            res.status(400).json({
+                status: 'fail',
+                error: err.message
+            })
+        }
+
+    } else {
         res.status(200).json({
-            status: 'success',
-            data: response,
-            saveToDB: newTranslation
-        })
-    }).catch(function (error) {
-        console.error(error);
-    });
+            status: 'cached',
+            data: searchResult[0]
+        });
+    }
 
 
 });
--- models/translation.js
+++ models/translation.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const translationSchema = new mongoose.Schema({
-    apiKey: {
+    // apiKey: {
+    //     type: String,
+    //     required: [true, 'Please provide apiKey to make a api call for translation.'],
+    // },
+    fromLanguage: {
         type: String,
-        required: [true, 'Please provide apiKey to make a api call for translation.'],
+        required: [true, 'Target language. Language codes to be used are the two-letter codes defined by the ISO 639-1:2002.']
     },
-    language: {
+    toLanguage: {
         type: String,
         required: [true, 'Target language. Language codes to be used are the two-letter codes defined by the ISO 639-1:2002.']
     },
@@ -13,6 +17,10 @@ const translationSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please provide text to translate.'],
         minlength: [2, 'Text should be at least two character']
+    },
+    translatedMessage: {
+        type: String,
+        required: [false, 'Message might not get translated everytime. Hence it is not required!']
     }
 }, { timestamps: true });
 
